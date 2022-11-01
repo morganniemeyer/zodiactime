e415c201f80c8e850f3f508a96e0f1811eb84255
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-01 12:04:20 -0500

    write test 2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,23 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const { zodiac } = require('../lib/zodiac-data.js');
-console.log(zodiac);
 describe('zodiac routes', () => {
   it('/zodiac should return a list of zodiac signs', async () => {
     const zList = await request(app).get('/zodiac');
     const expected = zodiac.map((z) => {
-      return { name: z.name, dates: z.dates };
+      return { sign: z.name, dates: z.dates };
     });
     expect(zList.body).toEqual(expected);
+  });
+
+  it('/zodiac/:id should return sign detail' async () => {
+    const response = await request(app).get('/zodiac/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(response.body).toEqual(aquarius);
   });
 });
