a42948ab44081ee18d2859b34ba381d9bf60d29f
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-10-31 20:02:05 -0500

    passing first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const { zodiac } = require('../lib/zodiac-data.js');
-
+console.log(zodiac);
 describe('zodiac routes', () => {
   it('/zodiac should return a list of zodiac signs', async () => {
     const zList = await request(app).get('/zodiac');
-    const expected = zodiac.map((zodiac) => {
-      return { name: zodiac.name, dates: zodiac.dates };
+    const expected = zodiac.map((z) => {
+      return { name: z.name, dates: z.dates };
     });
     expect(zList.body).toEqual(expected);
   });
